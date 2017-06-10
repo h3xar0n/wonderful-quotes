d37a7d90c4de268593e8a462fd76ea3b9892772d
h3xar0n <aron@glambyadelina.com>
2017-06-09 18:12:03 -0700

    Quotes now appearing

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
-    <div class="container">
-    
-    </div>
+  <div class="container">
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
+  </div>
 </template>
 
 <script>
-    export default {
-        
+  import QuoteGrid from './components/QuoteGrid.vue'
+
+  export default {
+    data: function() {
+      return {
+        quotes: [
+                  'Just a quote to see something'
+        ],
+        maxQuotes: 10
+      }
+    },
+    components: {
+      appQuoteGrid: QuoteGrid
     }
+  }
 </script>
 
 <style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+    <app-quote v-for="quote in quotes">
+      {{ quote }}
+    </app-quote>
+  </div>
+</template>
+
+<script type="text/javascript">
+  import Quote from './Quote.vue';
+
+  export default {
+    props: ['quotes'],
+    components: {
+      appQuote: Quote
+    }
+  }
+</script>
+
+<style type="text/css">
+  
+</style>
